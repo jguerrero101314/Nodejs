4a8c968a45a8d96493135037cdda4a3170aa3e3b
jguerrero101314 <joelg1014@hotmail.com>
2020-04-17 14:21:53 -0500

    29. Requerir paquetes - required

--- 03-basesNode/app.js
+++ 03-basesNode/app.js
@@ -1,7 +1,14 @@
-let base = 3;
+//requireds
+const fs = require('fs');
+let data = '';
+
+let base = 10;
 
 for (let i = 1; i <= 10; i++) {
-    console.log(`${base} * ${i} = ${base*i}`)
-
-
+    data += `${base} * ${i} = ${base*i}\n`;
 }
+// const data = new Uint8Array(Buffer.from('Hello Node.js'));
+fs.writeFile(`tablas/tabla-${base}.txt`, data, (err) => {
+    if (err) throw err;
+    console.log(`El archivo tabla-${base}.txt ha sido creado`);
+});
