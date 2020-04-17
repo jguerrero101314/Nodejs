c514f5dd15439fdf85c53ae7adbc632dddc7d315
jguerrero101314 <joelg1014@hotmail.com>
2020-04-17 15:19:45 -0500

    31. Recibir información desde línea de comando

--- 03-basesNode/app.js
+++ 03-basesNode/app.js
@@ -1,5 +1,11 @@
 const { crearArchivo } = require('./multiplicar/multiplicar');
-let base = '0';
+
+
+let argv = process.argv;
+let parametro = argv[2];
+console.log(parametro);
+let base = parametro.split('=')[1];
+console.log(base);
 
 
 crearArchivo(base).then(archivo => console.log(`Archivo creado: ${archivo}`)).catch(e => console.log(e));
