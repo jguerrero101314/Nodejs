2b8e7a9322d681e3d446f1f8e7073e2467d5c3f8
jguerrero101314 <joelg1014@hotmail.com>
2020-04-18 13:15:06 -0500

    36. Colores de la consola

--- 03-basesNode/app.js
+++ 03-basesNode/app.js
@@ -1,5 +1,6 @@
 const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar');
 const argv = require('./config/yargs').argv;
+const colors = require('colors');
 let comando = argv._[0];
 switch (comando) {
     case 'listar':
--- 03-basesNode/multiplicar/multiplicar.js
+++ 03-basesNode/multiplicar/multiplicar.js
@@ -1,6 +1,12 @@
 //requireds
 const fs = require('fs');
-let listarTabla = (base, limite = 10) => {
+const colors = require('colors');
+
+let listarTabla = (base, limite) => {
+    console.log('==================='.green);
+    console.log(`Tabla de ${base}`.green);
+    console.log('==================='.green)
+
     for (let i = 1; i <= limite; i++) {
         console.log(`${base} * ${i} = ${base*i}\n`);
     }
@@ -10,7 +16,7 @@ const crearArchivo = (base, limite) => {
 
     return new Promise((resolve, reject) => {
         if (!Number(base)) {
-            reject(`El valor introducido ${base} no es un numero`);
+            reject(`El valor introducido ${base} no es un numero`.red);
             return;
         }
         let data = '';
@@ -22,7 +28,7 @@ const crearArchivo = (base, limite) => {
             if (err)
                 reject(err)
             else
-                resolve(`tabla-${base}-al-${limite}.txt`);
+                resolve(`tabla-${base}-al-${limite}.txt`.green);
         });
     });
 }
