8efce74fefd799dad5c76032345d0e339e6bfcf3
jguerrero101314 <joelg1014@hotmail.com>
2020-04-18 12:16:12 -0500

    33. Yargs

--- 03-basesNode/app.js
+++ 03-basesNode/app.js
@@ -1,11 +1,25 @@
 const { crearArchivo } = require('./multiplicar/multiplicar');
+const argv = require('yargs')
+    .command('listar', 'Imprime en consola la tabla de multipilicar', {
+        base: {
+            demand: true,
+            alias: 'b'
+        },
+        limite: {
+            alias: 'l',
+            default: 10
+        }
+    })
+    .help()
+    .argv;
+
+let argv2 = process.argv;
+console.log('Limite', argv.limite);
+
+// let parametro = argv[2];
+// console.log(parametro);
+// let base = parametro.split('=')[1];
+// console.log(base);
 
 
-let argv = process.argv;
-let parametro = argv[2];
-console.log(parametro);
-let base = parametro.split('=')[1];
-console.log(base);
-
-
-crearArchivo(base).then(archivo => console.log(`Archivo creado: ${archivo}`)).catch(e => console.log(e));
+// crearArchivo(base).then(archivo => console.log(`Archivo creado: ${archivo}`)).catch(e => console.log(e));
