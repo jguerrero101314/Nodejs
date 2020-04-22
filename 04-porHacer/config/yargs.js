89dfe0a8acafc5e9db26629565a47aede6c6380d
jguerrero101314 <joelg1014@hotmail.com>
2020-04-22 14:58:00 -0500

    6 min
    48. Optimizaciones para la aplicación de
    Notas

--- 04-porHacer/config/yargs.js
+++ 04-porHacer/config/yargs.js
@@ -1,29 +1,23 @@
+const descripcion = {
+    demand: true,
+    alias: 'd',
+    descripcion: 'Descripcion de la tarea por hacer'
+};
+const completado = {
+    default: true,
+    alias: 'c',
+    descripcion: 'Marca como compleado o pendiente la tarea'
+}
 const argv = require('yargs')
     .command('crear', 'Crear un elemento por hacer', {
-        descripcion: {
-            demand: true,
-            alias: 'd',
-            descripcion: 'Descripcion de la tarea por hacer'
-        }
+        descripcion
     })
     .command('actualizar', 'Actualiza el estado completo de una tarea', {
-        descripcion: {
-            demand: true,
-            alias: 'd',
-            descripcion: 'Descripcion de la tarea por hacer'
-        },
-        completado: {
-            default: true,
-            alias: 'c',
-            descripcion: 'Marca como compleado o pendiente la tarea'
-        }
+        descripcion,
+        completado
     })
     .command('borrar', 'Borra un elemento por hacer', {
-        descripcion: {
-            demand: true,
-            alias: 'd',
-            descripcion: 'Descripcion de la tarea por hacer'
-        }
+        descripcion
     })
     .help()
     .argv;
