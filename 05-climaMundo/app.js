dbdaf1e4cd5985fb19bdd194367f217209aa871d
jguerrero101314 <joelg1014@hotmail.com>
2020-04-24 07:55:39 -0500

    56. Peticiones HTTP - Axios - Request

--- 05-climaMundo/app.js
+++ 05-climaMundo/app.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 const argv = require('yargs').options({
     direccion:{
         alias:'d',
@@ -6,4 +7,17 @@ const argv = require('yargs').options({
     }
 }).argv;
 
-console.log(argv.direccion);
+
+const endocedUlr = encodeURI(argv.direccion);
+console.log(endocedUlr);
+
+
+const instance = axios.create({
+    baseURL: `https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location=${endocedUlr}`,
+    headers: {'x-rapidapi-key': '5654fb123bmshb5fe2b181de1f47p183943jsn5f0e8a248f61'}
+  });
+  instance.get().then(resp=>{
+      console.log(resp.data.Results[0]);
+  }).catch(err => {
+      console.log('ERROR!!!!!',err);
+  })
