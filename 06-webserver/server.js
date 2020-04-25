0617361700d48e78ca43747435217fe6b4a98818
jguerrero101314 <joelg1014@hotmail.com>
2020-04-25 14:20:31 -0500

    65. Introducción a EXPRESS

---
+++ 06-webserver/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    // res.send('Hello World');
+    let salida = {
+        nombre: 'Joel',
+        edad: 22,
+        url: req.url
+    };
+    res.send(salida);
+});
+app.get('/data', (req, res) => {
+
+    res.send('Hola data');
+})
+app.listen(3000, () => {
+    console.log('Escuchando peticiones en el puerto 3000')
+})
