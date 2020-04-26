136660f5f38b3634645410d7397aecd507ea046b
jguerrero101314 <joelg1014@hotmail.com>
2020-04-26 11:14:03 -0500

    68. Handlebars

--- 06-webserver/server.js
+++ 06-webserver/server.js
@@ -2,16 +2,22 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname + '/public'));
+// Express HBS engine
+app.set('view engine', 'hbs');
 
-// app.get('/', (req, res) => {
-//      res.send('Hello World');
-//     let salida = {
-//         nombre: 'Joel',
-//         edad: 22,
-//         url: req.url
-//     };
-//     res.send(salida);
-// });
+app.get('/', (req, res) => {
+    //  res.send('Hello World');
+    // let salida = {
+    //     nombre: 'Joel',
+    //     edad: 22,
+    //     url: req.url
+    // };
+    //res.send(salida);
+    res.render('home', {
+        nombre: 'Joel',
+        anio: new Date().getFullYear()
+    });
+});
 
 app.listen(3000, () => {
     console.log('Escuchando peticiones en el puerto 3000')
