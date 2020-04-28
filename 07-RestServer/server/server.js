38cffd6e44e2b0cd99a03aab6194764a4238dd80
jguerrero101314 <joelg1014@hotmail.com>
2020-04-28 13:13:14 -0500

    80. Peticiones HTTP - GET - PUT - POST - DELETE

--- 07-RestServer/server/server.js
+++ 07-RestServer/server/server.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
-app.get('/', function(req, res) {
-    res.json('Hello World')
-})
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+    // parse application/json
+app.use(bodyParser.json())
+
+app.get('/usuario', function(req, res) {
+    res.json('get usuario')
+});
+app.post('/usuario', function(req, res) {
+    let body = req.body;
+    res.json({
+        persona: body
+    });
+});
+app.put('/usuario/:id', function(req, res) {
+    let id = req.params.id;
+    res.json({
+        id
+    });
+});
+app.delete('/usuario', function(req, res) {
+    res.json(' delete usuario');
+});
 
 app.listen(3000, () => {
     console.log('Escuchando puerto: ', 3000);
