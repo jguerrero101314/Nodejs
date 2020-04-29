b938b8db12f4c5a74f76e81d1f534e78dc179240
jguerrero101314 <joelg1014@hotmail.com>
2020-04-29 12:09:19 -0500

    83. Creando un archivo de configuración global

--- 07-RestServer/server/server.js
+++ 07-RestServer/server/server.js
@@ -1,3 +1,4 @@
+require('./config/config');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
@@ -34,6 +35,6 @@ app.delete('/usuario', function(req, res) {
     res.json(' delete usuario');
 });
 
-app.listen(3000, () => {
-    console.log('Escuchando puerto: ', 3000);
+app.listen(process.env.PORT, () => {
+    console.log('Escuchando puerto: ', process.env.PORT);
 })
