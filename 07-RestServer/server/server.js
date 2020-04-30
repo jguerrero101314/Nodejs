6f9f34e8893d2614b64e0b821a6614d3bbd28696
jguerrero101314 <joelg1014@hotmail.com>
2020-04-30 12:06:30 -0500

    91. Mongoose - Conectarnos a la base de datos

--- 07-RestServer/server/server.js
+++ 07-RestServer/server/server.js
@@ -1,5 +1,6 @@
 require('./config/config');
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const bodyParser = require('body-parser');
 
@@ -34,7 +35,14 @@ app.put('/usuario/:id', function(req, res) {
 app.delete('/usuario', function(req, res) {
     res.json(' delete usuario');
 });
+mongoose.connect('mongodb://localhost:27017/cafe', (err, res) => {
+
+    if (err) throw err;
+    console.log('Base de datos ONLINE');
+
+
+});
 
 app.listen(process.env.PORT, () => {
     console.log('Escuchando puerto: ', process.env.PORT);
-})
+});
