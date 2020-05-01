d6a11993c3d181c6c894c18c6edbe676dcee6442
jguerrero101314 <joelg1014@hotmail.com>
2020-05-01 13:58:26 -0500

    100. GET: Obtener todos los usuarios de forma paginada

--- 07-RestServer/server/routes/usuario.js
+++ 07-RestServer/server/routes/usuario.js
@@ -5,7 +5,26 @@ const bcrypt = require('bcrypt');
 const _ = require('underscore');
 
 app.get('/usuario', function(req, res) {
-    res.json('get usuario')
+    let desde = req.query.desde || 0;
+    desde = Number(desde);
+    let limite = req.query.limite || 5;
+    limite = Number(limite);
+
+    Usuario.find({})
+        .skip(desde)
+        .limit(limite)
+        .exec((err, usuarios) => {
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    err
+                });
+            }
+            res.json({
+                ok: true,
+                usuarios
+            })
+        })
 });
 app.post('/usuario', function(req, res) {
     let body = req.body;
