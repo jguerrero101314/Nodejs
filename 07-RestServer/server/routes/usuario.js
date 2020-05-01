89c95fa893c7de0b11a1b78e60c63d6e5f29469d
jguerrero101314 <joelg1014@hotmail.com>
2020-05-01 15:18:53 -0500

    103. Delete: Borrando un usuario de la base de datos

--- 07-RestServer/server/routes/usuario.js
+++ 07-RestServer/server/routes/usuario.js
@@ -77,7 +77,28 @@ app.put('/usuario/:id', function(req, res) {
 
 })
 
-app.delete('/usuario', function(req, res) {
-    res.json(' delete usuario');
+app.delete('/usuario/:id', function(req, res) {
+    let id = req.params.id;
+    Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        };
+        if (!usuarioBorrado) {
+            return res.status(400).json({
+                ok: false,
+                error: {
+                    message: 'Usuario no encontrado'
+                }
+            });
+        }
+        res.json({
+            ok: true,
+            usuario: usuarioBorrado
+        })
+    })
+
 });
 module.exports = app;
