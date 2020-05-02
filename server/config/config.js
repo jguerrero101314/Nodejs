912fc554dd64a1adc890e37633e4a08b4fab20ff
jguerrero101314 <joelg1014@hotmail.com>
2020-05-02 14:35:07 -0500

    RestServer -usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,14 @@
 //Puerto
 process.env.PORT = process.env.PORT || 3000;
+
+//entorno
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+//BD
+let urlDB;
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://Clyde:p0nN2IVhmrXZl9rs@cluster0-eqbnb.mongodb.net/Cafe';
+}
+
+process.env.URLDB = urlDB;
