589a2be51128293f12f75d98ddeb6f15c7a9ad6b
jguerrero101314 <joelg1014@hotmail.com>
2020-05-04 10:56:40 -0500

    120. Ordenando las rutas en nuestro servidor

--- 07-RestServer/server/server.js
+++ 07-RestServer/server/server.js
@@ -4,11 +4,13 @@ const mongoose = require('mongoose');
 const app = express();
 const bodyParser = require('body-parser');
 
+//configuracion global de rutas
+app.use(require('./routes/index'));
+
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
     // parse application/json
 app.use(bodyParser.json());
-app.use(require('./routes/usuario'));
 
 
 mongoose.connect(process.env.URLDB, {
