e3eff249742ef880488a68bbf5e77cbe914acf02
jguerrero101314 <joelg1014@hotmail.com>
2020-05-06 14:51:47 -0500

    132. Obtención del API Key y API Secret de Google

--- 07-RestServer/server/server.js
+++ 07-RestServer/server/server.js
@@ -3,13 +3,17 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const bodyParser = require('body-parser');
+const path = require('path');
 
 
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
-    // parse application/json
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
 app.use(bodyParser.json());
+
+//habilitar la carpeta public
+app.use(express.static(path.resolve(__dirname, '../public')));
 
 //configuracion global de rutas
 app.use(require('./routes/index'));
