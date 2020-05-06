deeb66b147d50f416d012c06812858c884cd90b3
jguerrero101314 <joelg1014@hotmail.com>
2020-05-06 15:31:55 -0500

    134. Crear un usuario personalizado en base a las credenciales de Google

--- 07-RestServer/server/routes/login.js
+++ 07-RestServer/server/routes/login.js
@@ -49,17 +49,75 @@ async function verify(token) {
         audience: process.env.CLIENT_ID
     });
     const payload = ticket.getPayload();
-    console.log(payload.name);
-    console.log(payload.email);
-    console.log(payload.picture);
+
+    return {
+        nombre: payload.name,
+        email: payload.email,
+        img: payload.picture,
+        google: true
+    }
 }
 
-app.post('/google', (req, res) => {
+app.post('/google', async(req, res) => {
     let token = req.body.idtoken;
-    verify(token);
-    res.json({
-        token
-    })
+    let googleUser = await verify(token)
+        .catch(e => {
+            return res.status(403).json({
+                ok: false,
+                err: e
+            });
+        });
+    Usuario.findOne({ email: googleUser.email }, (err, usuarioDB) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        };
+        if (usuarioDB) {
+            if (usuarioDB.google === false) {
+                return res.status(400).json({
+                    ok: false,
+                    err: {
+                        message: 'Debe de usar su autenticacion normal'
+                    }
+                });
+            } else {
+                let token = jwt.sign({
+                    usuario: usuarioDB
+                }, process.env.SEED, { expiresIn: process.env.CADUCIDAD_TOKEN });
+                return res.json({
+                    ok: true,
+                    usuario: usuarioDB,
+                    token
+                });
+            }
+        } else {
+            //si el usuario no existe en nuestra BD
+            let usuario = new Usuario();
+            usuario.nombre = googleUser.nombre;
+            usuario.email = googleUser.email;
+            usuario.img = googleUser.img;
+            usuario.google = true;
+            usuario.password = ':)';
+            usuario.save((err, usuarioDB) => {
+                if (err) {
+                    return res.status(500).json({
+                        ok: false,
+                        err
+                    });
+                };
+                let token = jwt.sign({
+                    usuario: usuarioDB
+                }, process.env.SEED, { expiresIn: process.env.CADUCIDAD_TOKEN });
+                return res.json({
+                    ok: true,
+                    usuario: usuarioDB,
+                    token
+                });
+            });
+        };
+    });
 });
 
 module.exports = app;
