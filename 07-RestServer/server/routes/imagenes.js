191c4866161060c542289bdf9e4e0a54082c51d4
jguerrero101314 <joelg1014@hotmail.com>
2020-05-09 15:44:32 -0500

    162. Mostrar imagen de usuario o producto

--- 07-RestServer/server/routes/imagenes.js
+++ 07-RestServer/server/routes/imagenes.js
@@ -2,16 +2,21 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 const path = require('path');
+const { verificaToken } = require('../middlewares/authentication');
 
 
-app.get('/imagen/:tipo/:img', (req, res) => {
+app.get('/imagen/:tipo/:img', verificaToken, (req, res) => {
     let tipo = req.params.tipo;
     let img = req.params.img;
 
-    let pathImg = `../../uploads/${tipo}/${img}`;
-    let noImagePath = path.resolve(__dirname, '../assets/no-image.jpg');
+    let pathImagen = path.resolve(__dirname, `../../uploads/${tipo}/${img}`);
+    if (fs.existsSync(pathImagen)) {
+        res.sendFile(pathImagen);
+    } else {
+        let noImagePath = path.resolve(__dirname, '../assets/no-image.jpg');
+        res.sendFile(noImagePath);
+    }
 
-    res.sendFile(noImagePath);
 });
 
 
