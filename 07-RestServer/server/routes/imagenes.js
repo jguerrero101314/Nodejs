0a5bb957464cb3eefc688d8c51cb083b448b1d9f
jguerrero101314 <joelg1014@hotmail.com>
2020-05-09 15:25:46 -0500

    161. Servicio para mostrar las imágenes

---
+++ 07-RestServer/server/routes/imagenes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const path = require('path');
+
+
+app.get('/imagen/:tipo/:img', (req, res) => {
+    let tipo = req.params.tipo;
+    let img = req.params.img;
+
+    let pathImg = `../../uploads/${tipo}/${img}`;
+    let noImagePath = path.resolve(__dirname, '../assets/no-image.jpg');
+
+    res.sendFile(noImagePath);
+});
+
+
+
+module.exports = app;
