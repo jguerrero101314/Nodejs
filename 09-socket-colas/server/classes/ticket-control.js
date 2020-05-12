b2d82b5e9f7cbe18131d3e651e764f6b25b48bbc
jguerrero101314 <joelg1014@hotmail.com>
2020-05-12 09:02:47 -0500

    186. Lógica: Siguiente ticket y centralizar la grabación

--- 09-socket-colas/server/classes/ticket-control.js
+++ 09-socket-colas/server/classes/ticket-control.js
@@ -7,20 +7,29 @@ class TicketControl {
         let data = require('../data/data.json');
         // console.log(data);
         if (data.hoy === this.hoy) {
-
+            this.ultimo = data.ultimo;
         } else {
             this.reiniciarConteo();
         }
     }
+    siguiente() {
+        this.ultimo += 1;
+
+    }
     reiniciarConteo() {
+        this.ultimo = 0;
+        console.log('Se ha inicializado el sistema');
+        this.grabarArchivo();
+
+        retur `Ticket ${this.ultimo}`;
+    }
+    grabarArchivo() {
         let jsonData = {
             ultimo: this.ultimo,
             hoy: this.hoy
         };
         let jsonDataString = JSON.stringify(jsonData);
         fs.writeFileSync('./server/data/data.json', jsonDataString);
-        console.log('Se ha inicializado el sistema');
-
     }
 }
 
