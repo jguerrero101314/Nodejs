c15df765b30459e19d5e2aa9bbad804beeffa676
jguerrero101314 <joelg1014@hotmail.com>
2020-05-12 08:58:43 -0500

    185. ES6 Classes - Clase para centralizar la lógica

---
+++ 09-socket-colas/server/classes/ticket-control.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+class TicketControl {
+    constructor() {
+        this.ultimo = 0;
+        this.hoy = new Date().getDate();
+
+        let data = require('../data/data.json');
+        // console.log(data);
+        if (data.hoy === this.hoy) {
+
+        } else {
+            this.reiniciarConteo();
+        }
+    }
+    reiniciarConteo() {
+        let jsonData = {
+            ultimo: this.ultimo,
+            hoy: this.hoy
+        };
+        let jsonDataString = JSON.stringify(jsonData);
+        fs.writeFileSync('./server/data/data.json', jsonDataString);
+        console.log('Se ha inicializado el sistema');
+
+    }
+}
+
+module.exports = {
+    TicketControl
+}
