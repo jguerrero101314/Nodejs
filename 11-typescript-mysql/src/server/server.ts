75550054b084bacba5fae0142cbc48dfc90e5592
jguerrero101314 <joelg1014@hotmail.com>
2020-05-15 12:52:29 -0500

    221. Desplegar el public folder

--- 11-typescript-mysql/src/server/server.ts
+++ 11-typescript-mysql/src/server/server.ts
@@ -1,4 +1,5 @@
 import express = require("express");
+import path = require("path");
 
 export default class Server {
   public app: express.Application;
@@ -11,7 +12,12 @@ export default class Server {
   static init(puerto:number){
       return new Server(puerto);
   }
+  private publicFolder(){
+    const publicPath = path.resolve(__dirname,'../public');
+    this.app.use(express.static(publicPath));
+  }
   start(callback:Function){
     this.app.listen(this.port, callback());
+    this.publicFolder();
   }
 }
