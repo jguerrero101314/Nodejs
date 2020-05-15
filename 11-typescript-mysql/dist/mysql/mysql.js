419a6e31e4d78c4fc68e0069dddb3a819dd28321
jguerrero101314 <joelg1014@hotmail.com>
2020-05-15 13:47:18 -0500

    225. Clase para conectarse a MySQL

---
+++ 11-typescript-mysql/dist/mysql/mysql.js
@@ -0,0 +1,28 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const mysql = require("mysql");
+class MySQL {
+    constructor() {
+        this.conectado = false;
+        console.log('Clase inicializada');
+        this.cnn = mysql.createConnection({
+            host: 'localhost',
+            user: 'node_user',
+            password: '123456',
+            database: 'node_db'
+        });
+        this.conectarDB();
+        // this.cnn.connect();
+    }
+    conectarDB() {
+        this.cnn.connect((err) => {
+            if (err) {
+                console.log(err.message);
+                return;
+            }
+            this.conectado = true;
+            console.log('Base de datos online');
+        });
+    }
+}
+exports.default = MySQL;
